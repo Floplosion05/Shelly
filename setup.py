7fd532d4b837abce85517805a681a51639a6d505
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2020-10-21 18:27:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="shelly-test",
+    version="0.0.2",
+    author="Floplosion05",
+    author_email="florfuchs2005@gmail.com",
+    license='MIT',
+    description="A test package for shelly devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    keywords='shelly login shelly-login'
+    url="https://github.com/Floplosion05/Shelly",
     packages=setuptools.find_packages(),
     classifiers=[
+        'Intended Audience :: Developers',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    install_requires=[  
+        'requests>=2.24.0',
+        'passlib >= 1.7.4'
+    ],
+    python_requires='>=3.6'
 )
